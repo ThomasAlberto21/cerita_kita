14740e65ea3bfa35dcd68b9ad086e83796bffb83
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-03-16 21:51:20 +0700

    Membuat Button Login

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,7 +1,9 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navbarMenu = document.querySelector('.navbar-menu');
+const btnLogin = document.querySelector('.btn-login')
 
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('active');
   navbarMenu.classList.toggle('active');
+  btnLogin.classList.toggle('active')
 });
