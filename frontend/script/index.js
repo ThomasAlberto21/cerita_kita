58d3e723c390b1545a31e3ca0a06ad3abf2649dd
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-03-17 11:48:45 +0700

    Membuat Form Login Dan Register

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,9 +1,12 @@
+// Hamburger Menu
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navbarMenu = document.querySelector('.navbar-menu');
-const btnLogin = document.querySelector('.btn-login')
+const btnLogin = document.querySelector('.btn-login');
 
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('active');
   navbarMenu.classList.toggle('active');
-  btnLogin.classList.toggle('active')
+  btnLogin.classList.toggle('active');
 });
+
+
