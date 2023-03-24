72e62b3ccf9ac366152d7dbc601e2168232b5eff
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-03-24 19:47:34 +0700

    Membuat Section Activity

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -9,4 +9,31 @@ hamburgerMenu.addEventListener('click', () => {
   btnLogin.classList.toggle('active');
 });
 
+const carousel = document.querySelector('.carousel-container');
+const cards = document.querySelectorAll('.card');
 
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 3;
+  carousel.scrollLeft = scrollLeft - walk;
+});
