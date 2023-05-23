bb21b4daf5888cc6d18dc9508544b1d7be0467ae
Muhammad Ridho Adhari <110064566+Jnosvb@users.noreply.github.com>
2023-05-23 08:02:36 +0700

    Chnge pass

--- backend/db/cerita_kita.sql
+++ backend/db/cerita_kita.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Mar 2023 pada 07.09
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.0.19
+-- Waktu pembuatan: 23 Bulan Mei 2023 pada 02.58
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `activity`
+--
+
+CREATE TABLE `activity` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `foto` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `activity`
+--
+
+INSERT INTO `activity` (`id`, `judul`, `keterangan`, `tanggal`, `foto`) VALUES
+(1, 'Melancar', 'Gabut Amat', '2023-05-18 13:11:09', 'images\\activity_img_1.png'),
+(2, 'Melancar 2', 'Gabut Amat 2', '2023-05-18 13:16:28', 'images\\activity_img_2.png'),
+(3, 'Melancar 3', 'Gabut 3', '2023-05-18 13:17:09', 'images\\riam_merasap.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
@@ -31,19 +54,25 @@ CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `pass` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`user_id`, `email`, `username`, `pass`) VALUES
-(1, 'edo@gmail.com', 'jen', 221);
+INSERT INTO `user` (`user_id`, `email`, `username`, `password`) VALUES
+(1, 'edo@gmail.com', 'Jennv', '$2y$10$wdWIPbm/Y6OORcVYR7HRMOkzllJVdqUKKbFu7tD9EZNmvYtCUPfnm');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `user`
@@ -54,6 +83,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
