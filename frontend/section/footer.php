85f180497ffb467bc167d33a499fa846475a28a8
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-06 13:11:18 +0700

    .

--- frontend/section/footer.php
+++ frontend/section/footer.php
@@ -3,24 +3,24 @@
       <div class="title">
         <h1>Contact Us</h1>
         <p>Contact us with any suggestions and input for our community</p>
-        <a href="https://www.instagram.com/lvn.jenn/" class="instagram"><i class="fa-brands fa-instagram fa-2xl"></i></a>
+        <a href="https://www.instagram.com/adv.ceritakita/" class="instagram"><i class="fa-brands fa-instagram fa-2xl"></i></a>
         <a href=""><i class="fa-regular fa-envelope fa-2xl"></i></a>
         <a href=""><i class="fa-brands fa-whatsapp fa-2xl"></i></a>
       </div>
       <form action="">
         <div class="contact-form">
           <label for="email">Email</label> <br />
-          <input type="text" name="email_from" id="email" placeholder="Masukkan Email Anda" require/>
+          <input type="text" name="email_from" id="email" placeholder="Masukkan Email Anda" require />
         </div>
 
         <div class="contact-form">
           <label for="email">Nama</label> <br />
-          <input type="text" name="nama" id="nama" placeholder="Masukkan Nama Anda"/>
+          <input type="text" name="nama" id="nama" placeholder="Masukkan Nama Anda" />
         </div>
 
         <div class="contact-form">
           <label for="email">Pesan</label> <br />
-          <textarea name="pesan" id="" rows="12" placeholder="Masukkan Pesan Anda" ></textarea>
+          <textarea name="pesan" id="" rows="12" placeholder="Masukkan Pesan Anda"></textarea>
         </div>
         <button type="submit">Kirim Pesan</button>
       </form>
@@ -32,6 +32,7 @@
     </div>
 
     <script src="script/index.js"></script>
-    
-  </body>
-</html>
+
+    </body>
+
+    </html>
