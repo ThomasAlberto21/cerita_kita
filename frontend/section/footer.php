5f9a6e98ce265f4f1cbf192c38898ade96e13b06
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-20 08:36:08 +0700

    .

--- frontend/section/footer.php
+++ frontend/section/footer.php
@@ -4,8 +4,8 @@
         <h1>Contact Us</h1>
         <p>Contact us with any suggestions and input for our community</p>
         <a href="https://www.instagram.com/adv.ceritakita/" class="instagram"><i class="fa-brands fa-instagram fa-2xl"></i></a>
-        <a href=""><i class="fa-regular fa-envelope fa-2xl"></i></a>
-        <a href=""><i class="fa-brands fa-whatsapp fa-2xl"></i></a>
+        <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=new"><i class="fa-regular fa-envelope fa-2xl"></i></a>
+        <a href="https://wa.me/085752461241"><i class="fa-brands fa-whatsapp fa-2xl"></i></a>
       </div>
       <form action="../../backend/proses/mail.php" method="POST">
         <div class="contact-form">
